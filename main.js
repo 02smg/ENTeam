5c75b3dddf2ccf10ad0d2422ad9d496ff1aafc84
Song Mingeun <82743622+02smg@users.noreply.github.com>
2025-09-21 19:56:40 +0900

    Update main.js

--- main.js
+++ main.js
@@ -18,9 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll("a[href^='#']").forEach((anchor) => {
     anchor.addEventListener("click", function (e) {
       e.preventDefault();
-      const target = document.querySelector(
-        this.getAttribute("href")
-      );
+      const target = document.querySelector(this.getAttribute("href"));
       if (target) {
         target.scrollIntoView({ behavior: "smooth" });
       }
